431e85dee06d2b9288b8fe2ce5b137ff0f256ba2
Adam Ross Benton <adamrossbenton@gmail.com>
2021-11-10 22:30:02 -0800

    moving bookmark and connection to models folder

--- server.js
+++ server.js
@@ -1,23 +1,10 @@
 ////////////////////////////////////////////////
 require("dotenv").config()
 const express = require("express")
-const mongoose = require("mongoose")
+const Bookmark = require("./models/bookmark")
 
-const {PORT, DB_URL} = process.env;
+const {PORT} = process.env;
 const app = express()
-
-////////////////////////////////////////////////
-// DB
-////////////////////////////////////////////////
-mongoose.connect(DB_URL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-})
-
-mongoose.connection
-    .on("open", () => console.log("Connected to DB"))
-    .on("open", () => console.log("Disconnected from DB"))
-    .on("error", (err) => console.log(err))
 
 ////////////////////////////////////////////////
 // Routes
