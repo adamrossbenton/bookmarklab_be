64eb4c2d08d9a82ce7aa491b1923a02d2172610d
Adam Ross Benton <adamrossbenton@gmail.com>
2021-11-12 16:41:37 -0800

    moving routes from server to controller

--- controllers/bookmarks.js
+++ controllers/bookmarks.js
@@ -16,7 +16,45 @@ const router = express.Router()
 // Routes
 ////////////////////////////////////////////////
 
+// Index
+router.get("/", async (req,res) => {
+    try {
+        res.json(await Bookmark.find({}))
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
 
+// Destroy
+router.delete("/:id", async (req,res) => {
+    try {
+        res.json(await Bookmark.findByIdAndRemove(req.params.id))
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
+
+// Update
+router.put("/:id", async (req,res) => {
+    try {
+        res.json(await Bookmark.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new: true}
+        ))
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
+
+// Create
+router.post("/", async (req,res) => {
+    try {
+        res.json(await Bookmark.create(req.body))
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
 
 ////////////////////////////////////////////////
 // Export
