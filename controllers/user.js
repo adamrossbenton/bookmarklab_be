b5c31b0c9be9d02d2fe766ae75b77437df47ce45
Adam Ross Benton <adamrossbenton@gmail.com>
2021-11-13 12:22:42 -0800

    setting up user router

--- controllers/user.js
+++ controllers/user.js
@@ -3,3 +3,25 @@
 ////////////////////////////////////////////////
 const express = require("express")
 const bcrypt = require("bcryptjs")
+const User = require("../models/user")
+
+const router = express.Router()
+
+////////////////////////////////////////////////
+// Routes
+////////////////////////////////////////////////
+
+// Signup
+// router.get("/signup", (req,res) => {
+    
+// })
+
+router.post("/signup", async (req,res) => {
+    req.body.password = await bcrypt.hash(
+        req.body.password,
+        await bcrypt.genSalt(10)
+    )
+    User.create(req.body, (err,user) => {
+        
+    })
+})
